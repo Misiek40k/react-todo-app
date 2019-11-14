580b7ae5ac0f2ad8c5fc4814f7a885a63364c3cc
Misiek40k <comanderrr@gmail.com>
2019-11-14 22:16:39 +0100

    Add Hero and List components with styles

---
+++ src/components/List/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Hero from '../Hero/Hero';
+
+import styles from './List.scss'
+
+export default class List extends Component {
+    render() {
+        return (
+            <section className={styles.component}>
+                <Hero/>
+            </section>
+        )
+    }
+}
