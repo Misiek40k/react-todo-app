aa3f50fa6e4dd9494d1d29fd740e2e2f7bed411d
Misiek40k <comanderrr@gmail.com>
2019-12-07 16:49:19 +0100

    Change App.js title and subtitle

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,10 +8,10 @@ class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello World</h2>
+        <h1 className={styles.title}>My React Todo App</h1>
+        <h2 className={styles.subtitle}>with lists, columns and cards</h2>
 
-        <List title={['Things to do ', <sup key='1'>soon!</sup>]} />
+        <List title={['Things to do ', <sup key='1'>soon!</sup>]}/>
       </main>
     )
   }
