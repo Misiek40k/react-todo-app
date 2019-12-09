67473a345b4e37eff49b6b8f9e182d48cfc02bfd
Misiek40k <comanderrr@gmail.com>
2019-12-09 16:12:05 +0100

    Add getting listData from redux state

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 import Hero from '../Hero/Hero';
-import Column from '../Column/Column';
-import Creator from '../Creator/Creator';
+// import Column from '../Column/Column';
+// import Creator from '../Creator/Creator';
 
 import styles from './List.scss';
 
@@ -42,20 +43,21 @@ export default class List extends Component {
     }
 
     render() {
+        const { title, image, description } = this.props;
         return (
             <section className={styles.component}>
-                <Hero titleText={this.props.title} heroImage={this.props.image} />
+                <Hero titleText={title} heroImage={image} />
                 <div className={styles.description}>
-                    {this.props.description}
+                    {ReactHtmlParser(description)}
                 </div>
-                <div className={styles.columns}>
+                {/* <div className={styles.columns}>
                     {this.state.columns.map(({ key, ...columnProps }) => (
                         <Column key={key} {...columnProps} />
                     ))}
                 </div>
                 <div className={styles.creator}>
                     <Creator text={settings.cardCreatorText} action={title => this.addColumn(title)} />
-                </div>
+                </div> */}
             </section>
         );
     }
