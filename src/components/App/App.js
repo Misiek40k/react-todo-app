699e35f92d34bcc6ff19b1f3d73f81f059aacdfd
Misiek40k <comanderrr@gmail.com>
2019-12-09 12:29:00 +0100

    Add eslint and husky

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-import {pageContents, listData} from '../../data/dataStore';
+import { pageContents, listData } from '../../data/dataStore';
 import List from '../List/List';
 
 import styles from './App.scss';
 
 class App extends React.Component {
-  render() {
-    return (
-      <main className={styles.component}>
-        <h1 className={styles.title}>{pageContents.title}</h1>
-        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+    render() {
+        return (
+            <main className={styles.component}>
+                <h1 className={styles.title}>{pageContents.title}</h1>
+                <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
 
-        <List {...listData}/>
-      </main>
-    )
-  }
+                <List {...listData} />
+            </main>
+        );
+    }
 }
 
 export default App;
