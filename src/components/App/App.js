2b6f846721d9be8d7c4dbe72a621e3879d7293f9
Misiek40k <comanderrr@gmail.com>
2019-12-23 21:31:00 +0100

    Add Faq component and Add Faq to routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import MainLayout from '../MainLayout/MainLayout';
+import Faq from '../Faq/Faq';
 
 const App = () => (
     <BrowserRouter>
@@ -11,6 +12,7 @@ const App = () => (
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/info' component={Info} />
+                <Route exact path='/faq' component={Faq} />
             </Switch>
         </MainLayout>
     </BrowserRouter>
